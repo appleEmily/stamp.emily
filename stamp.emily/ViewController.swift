2b738cf1dd99d6b339cd9f7bb452fea7a4509259
appleEmily <emiri3128@gmail.com>
2019-10-10 02:08:20 +0900

    Initial Commit

---
+++ stamp.emily/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stamp.emily
+//
+//  Created by 野崎絵未里 on 2019/10/10.
+//  Copyright © 2019 emily.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
